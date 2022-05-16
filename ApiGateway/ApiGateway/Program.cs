e0883c2963256e796c4b574ee5dd59a7e978b4dd
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-05-16 15:30:23 +0200

    Initialise project and add ocelot
    Initialise asp.net 6 project
    Add basic ocelot.json file and add it in Program.cs
    Add Ocelot nuget package to project

---
+++ ApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", false, true);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ApiGateway", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiGateway v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
+
+app.Run();
+
