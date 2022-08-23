93eafc885275a75dec8844c3cc97b0aaddfe1d20
Nikola <nikolakabasaj6@gmail.com>
2022-08-23 15:10:15 +0200

    feat: monitoring added

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,5 +1,13 @@
 ﻿using Microsoft.OpenApi.Models;
 using Ocelot.Middleware;
+using OpenTracing;
+using Jaeger.Reporters;
+using Jaeger;
+using Jaeger.Senders.Thrift;
+using Jaeger.Samplers;
+using OpenTracing.Contrib.NetCore.Configuration;
+using OpenTracing.Util;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +21,33 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "ApiGateway", Version = "v1" });
 });
+
+builder.Services.AddOpenTracing();
+
+builder.Services.AddSingleton<ITracer>(sp =>
+{
+    var serviceName = sp.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+    var reporter = new RemoteReporter.Builder()
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSender(new UdpSender("host.docker.internal", 6831, 0))
+                    .Build();
+    var tracer = new Tracer.Builder(serviceName)
+        // The constant sampler reports every span.
+        .WithSampler(new ConstSampler(true))
+        // LoggingReporter prints every reported span to the logging framework.
+        .WithLoggerFactory(loggerFactory)
+        .WithReporter(reporter)
+        .Build();
+
+    GlobalTracer.Register(tracer);
+
+    return tracer;
+});
+
+builder.Services.Configure<HttpHandlerDiagnosticOptions>(options =>
+        options.OperationNameResolver =
+            request => $"{request.Method.Method}: {request?.RequestUri?.AbsoluteUri}");
 
 var app = builder.Build();
 
@@ -29,6 +64,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Prometheus metrics
+app.UseMetricServer();
 
 app.UseOcelot().Wait();
 
